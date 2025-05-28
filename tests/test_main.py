88820c29a87f97c80ca499eaa96c59485244b530
jeanPMartins <jean.martins008@gmail.com>
2025-05-28 18:20:06 -0300

    refactor: tests and requirements

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,13 @@
-from http import HTTPStatus  
-
+from http import HTTPStatus
 from fastapi.testclient import TestClient
 
 from main import app
 
-def test_root_deve_retornar_ok():  
-    client = TestClient(app)  
 
-    response = client.get('/home')  
+def test_root_deve_retornar_ok():
+    client = TestClient(app)
 
-    assert response.status_code == HTTPStatus.OK  
-    assert response.json() == {"message": "API funcionando!"} 
+    response = client.get("/home")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "API funcionando!"}
